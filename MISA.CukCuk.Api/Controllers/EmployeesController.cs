b55285cef2e57de7ae3900a2aeae4cea395594b6
Ho Nam <hovannamedutt@gmail.com>
2021-01-09 11:50:57 +0700

    Add project files.

---
+++ MISA.CukCuk.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,58 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MISA.CukCuk.Api.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISA.CukCuk.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        /// <summary>
+        /// Lấy dữ liệu nhân viên
+        /// </summary>
+        /// <returns></returns>
+        /// CreatedBy: HVNam (1/9/2020)
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                // Kết nối tới Database: 
+                var connectionString = "Host=103.124.92.43;" +
+                    "Port=3306;" +
+                    "Database=MISACukCuk_MF656_HVNAM;" +
+                    "User Id=nvmanh;" +
+                    "Password=12345678";
+                IDbConnection dbConnection = new MySqlConnection(connectionString);
+                // Lấy dữ liệu từ Database:
+                var employees = dbConnection.Query<Employee>("Proc_GetEmployees", commandType:CommandType.StoredProcedure);
+                // Trả lại dữ liệu cho Client:
+                return Ok(employees);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Post([FromBody] string name)
+        {
+            return StatusCode(201,name);
+        }
+    }
+}
---
+++ MISA.CukCuk.Api/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISA.CukCuk.Api.Models
+{
+    public class Employee
+    {
+        public Guid EmployeeId { get; set; }
+        public string EmployeeCode { get; set; }
+        public string FullName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public int? Gender { get; set; }
+        public string IdentityNumber { get; set; }
+        public string IdentityDate { get; set; }
+        public string IdentityPlace { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string PersonalTaxCode { get; set; }
+        public Double? Salary { get; set; }
+        public DateTime? JoinDate { get; set; }
+        public int WorkStatus { get; set; }
+        public Guid? DepartmentId { get; set; }
+        public Guid? PositionId { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
